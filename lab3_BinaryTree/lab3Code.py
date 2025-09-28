66fdc012cb55fa243d254419518bc53266562898
IMaSKoT <maxsedov99@gmail.com>
2025-09-28 14:20:42 +0300

    add lab3_BinaryTree

---
+++ lab3_BinaryTree/lab3Code.py
@@ -0,0 +1,47 @@
+import types
+
+
+def left_branch17(root: int) -> int:
+    """
+    Вычисление значения левой ветки
+    :param root: Значение корня этой ветки
+    :return: Вычисляет значение ветки: (root-4)**2
+    """
+    return (root - 4) ** 2
+
+
+def right_branch17(root: int) -> int:
+    """
+    Вычисление значения правой ветки
+    :param root: Значение корня этой ветки
+    :return: Вычисляет значение ветки: (root+3)*2
+    """
+    return (root + 3) * 2
+
+
+def gen_bin_tree(height=4, root=17, l_b=left_branch17, r_b=right_branch17):
+    """
+    Построение дерева на основе введенных высоты, корня и правил вычисления веток
+    :param height: Значение высоты дерева
+    :param root: Значение корня дерева
+    :param l_b: Функция, вычисляющая левую ветку
+    :param r_b: Функция, вычисляющая правую ветку
+    :return: Бинарное дерево; если же данные введены неправильно, то None
+    """
+    # Проверка на корректно введенные данные
+    if isinstance(height, int) or not isinstance(root, int) or not isinstance(l_b, types.FunctionType) or not (
+            r_b, types.FunctionType):
+        # Проверка на корректно введенную функцию
+        try:
+            left_branch, right_branch = l_b(root), r_b(root)
+        except:
+            return
+        # Реализация построения бинарного дерева с помощью рекурсии
+        # Постепенное уменьшение высоты, пока значение height не будет равно нулю
+        if height <= 0:
+            return {f'{root}': []}
+        else:
+            return {f'{root}': [gen_bin_tree(height - 1, left_branch), gen_bin_tree(height - 1, right_branch)]}
+    else:
+        return
+print(gen_bin_tree())
---
+++ lab3_BinaryTree/lab3Test.py
@@ -0,0 +1,40 @@
+import unittest
+import lab3Code as main
+
+
+class Testgen_bin_tree(unittest.TestCase):
+    # Обычный тест с height = 0
+    def test1(self):
+        self.assertEqual(main.gen_bin_tree(0, 17, l_b=main.left_branch17, r_b=main.right_branch17),
+                         {'17': []})
+
+    # Обычный тест с height = 1
+    def test2(self):
+        self.assertEqual(main.gen_bin_tree(1, 17, l_b=main.left_branch17, r_b=main.right_branch17),
+                         {'17': [{'169': []}, {'40': []}]})
+
+    # Обычный тест с root = 1, height = 1
+    def test3(self):
+        self.assertEqual(main.gen_bin_tree(1, 1, l_b=main.left_branch17, r_b=main.right_branch17),
+                         {'1': [{'9': []}, {'8': []}]})
+
+    # Тест на неправильно введенный height
+    def test4(self):
+        self.assertEqual(main.gen_bin_tree('', 17, l_b=main.left_branch17, r_b=main.right_branch17), None)
+
+    # Тест на неправильно введенный root
+    def test5(self):
+        self.assertEqual(main.gen_bin_tree(3, [], l_b=main.left_branch17, r_b=main.right_branch17), None)
+
+    # Тест на неправильно введенный l_b
+    def test6(self):
+        self.assertEqual(main.gen_bin_tree(3, 17, l_b=None, r_b=main.right_branch17), None)
+
+    # Тест на неправильно введенный r_b
+    def test7(self):
+        self.assertEqual(main.gen_bin_tree(3, 17, l_b=main.left_branch17, r_b=print()), None)
+
+    # Тест на отрицательый height
+    def test8(self):
+        self.assertEqual(main.gen_bin_tree(-1, 17, l_b=main.left_branch17, r_b=main.right_branch17),
+                         {'17': []})
