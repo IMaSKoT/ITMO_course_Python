a88e307f03b0042c50d53ca92817d96a186c891f
IMaSKoT <maxsedov99@gmail.com>
2025-10-13 22:24:27 +0300

    visual change

--- lab4_FactorialTime/lab4Code.py
+++ lab4_FactorialTime/lab4Code.py
@@ -1,7 +1,8 @@
 import timeit
 import matplotlib.pyplot as plt
 from functools import lru_cache
-@lru_cache()
+
+
 def fact_recursive(n: int) -> int:
     """
     Рекурсивный факториал
@@ -12,7 +13,7 @@ def fact_recursive(n: int) -> int:
         return 1
     return n * fact_recursive(n - 1)
 
-@lru_cache()
+
 def fact_iterative(n: int) -> int:
     """
     Нерекурсивный факториал
@@ -24,10 +25,12 @@ def fact_iterative(n: int) -> int:
         res *= i
     return res
 
+
 def benchmark(func, n, number=1, repeat=5):
     """Возвращает среднее время выполнения func(n)"""
     times = timeit.repeat(lambda: func(n), number=number, repeat=repeat)
     return min(times)
+
 
 def main():
     test_data = list(range(10, 300, 10))
@@ -36,8 +39,8 @@ def main():
     res_iterative = []
 
     for n in test_data:
-      res_recursive.append(benchmark(fact_recursive, n, repeat=5, number=1000))
-      res_iterative.append(benchmark(fact_iterative, n, repeat=5, number=1000))
+        res_recursive.append(benchmark(fact_recursive, n, repeat=5, number=1000))
+        res_iterative.append(benchmark(fact_iterative, n, repeat=5, number=1000))
 
     # Визуализация
     plt.plot(test_data, res_recursive, label="Рекурсивный")
