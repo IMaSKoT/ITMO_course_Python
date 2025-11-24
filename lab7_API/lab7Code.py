6056dfb8ba81cc241f1322f5a7ee00031db2d153
IMaSKoT <maxsedov99@gmail.com>
2025-11-24 14:26:51 +0300

    lab7 visual change

--- lab7_API/lab7Code.py
+++ lab7_API/lab7Code.py
@@ -5,7 +5,8 @@ import sys
 import io
 import functools
 
-#1 задание: Реализация декоратора logger
+
+# 1 задание: Реализация декоратора logger
 def logger(func=None, *, handle=sys.stdout):
     """
     Декоратор logger для записи информации и ошибок
@@ -17,7 +18,7 @@ def logger(func=None, *, handle=sys.stdout):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        #Запись начала работа функции
+        # Запись начала работа функции
         func_start = f"Запуск функции {func.__name__} с аргументами {args} {kwargs}"
         if isinstance(handle, logging.Logger):
             handle.info(func_start)
@@ -25,7 +26,7 @@ def logger(func=None, *, handle=sys.stdout):
             handle.write(f"INFO: {func_start}\n")
         try:
             result = func(*args, **kwargs)
-            #Запись успешного окончания работа функции
+            # Запись успешного окончания работа функции
             func_end = f"Функция {func.__name__} успешно завершена. Результат: {result}"
             if isinstance(handle, logging.Logger):
                 handle.info(func_end)
@@ -33,15 +34,18 @@ def logger(func=None, *, handle=sys.stdout):
                 handle.write(f"INFO: {func_end}\n")
             return result
         except Exception as e:
-            #Запись неуспешного окончания работы функции
+            # Запись неуспешного окончания работы функции
             func_error = f"Ошибка в функции {func.__name__}: {type(e).__name__} {e}"
             if isinstance(handle, logging.Logger):
                 handle.error(func_error)
             else:
                 handle.write(f"ERROR: {func_error}\n")
             raise e
+
     return wrapper
-#2 и 3 задание: Реализация функции get_currencies и обертка декоратором
+
+
+# 2 и 3 задание: Реализация функции get_currencies и обертка декоратором
 @logger(handle=sys.stdout)
 def get_currencies(currency_codes: list, url: str = "https://www.cbr-xml-daily.ru/daily_json.js") -> dict:
     """
@@ -75,37 +79,44 @@ def get_currencies(currency_codes: list, url: str = "https://www.cbr-xml-daily.r
 
     except ValueError:
         raise ValueError("Пришел некорректный JSON")
-#Задание 4: Реализация файл-логирования
-#Создаем переменную file_logger
+
+
+# Задание 4: Реализация файл-логирования
+# Создаем переменную file_logger
 file_logger = logging.getLogger("currency_file")
-#Настраиваем на обычную запись
+# Настраиваем на обычную запись
 file_logger.setLevel(logging.INFO)
-#Настраиваем место,куда будем записывать
+# Настраиваем место,куда будем записывать
 file_handler = logging.FileHandler("currencies.log", mode='w', encoding='utf-8')
-#Настраиваем форматирование, чтобы добавить WARNING и CRITICAL
+# Настраиваем форматирование, чтобы добавить WARNING и CRITICAL
 formatter = logging.Formatter('%(levelname)s: %(message)s')
 file_handler.setFormatter(formatter)
 
 file_logger.addHandler(file_handler)
 
-#Тестируем наш file_logger используя функцию get_currencies
+
+# Тестируем наш file_logger используя функцию get_currencies
 @logger(handle=file_logger)
 def get_currencies_to_file(currency_codes):
     return get_currencies.__wrapped__(currency_codes)
+
+
 try:
-    get_currencies_to_file(['USD','EUR'])
+    get_currencies_to_file(['USD', 'EUR'])
 except Exception as e:
     pass
-#Задание 5: Реализуем демонстрационный пример
+# Задание 5: Реализуем демонстрационный пример
 
 # Создадим отдельный math_logger для будущей функции
 math_logger = logging.getLogger("quadratic.log")
 math_logger.setLevel(logging.DEBUG)
-math_handler = logging.FileHandler("quadratic.log",mode='w',encoding='utf-8')
+math_handler = logging.FileHandler("quadratic.log", mode='w', encoding='utf-8')
 formatter = logging.Formatter('%(levelname)s: %(message)s')
 math_handler.setFormatter(formatter)
 math_logger.addHandler(math_handler)
-#Сама функция
+
+
+# Сама функция
 @logger(handle=math_logger)
 def solve_quadratic(a, b, c):
     math_logger.info(f"Solving equation: {a}x^2 + {b}x + {c} = 0")
@@ -121,7 +132,7 @@ def solve_quadratic(a, b, c):
         math_logger.error("Coefficient 'a' cannot be zero")
         raise ValueError("a cannot be zero")
 
-    d = b*b - 4*a*c
+    d = b * b - 4 * a * c
     math_logger.debug(f"Discriminant: {d}")
 
     if d < 0:
@@ -129,30 +140,39 @@ def solve_quadratic(a, b, c):
         return None
 
     if d == 0:
-        x = -b / (2*a)
+        x = -b / (2 * a)
         math_logger.info("One real root")
         return (x,)
 
-    root1 = (-b + math.sqrt(d)) / (2*a)
-    root2 = (-b - math.sqrt(d)) / (2*a)
+    root1 = (-b + math.sqrt(d)) / (2 * a)
+    root2 = (-b - math.sqrt(d)) / (2 * a)
     math_logger.info("Two real roots computed")
     return root1, root2
-#Тестирование этой функции
 
 
-#INFO
-try: solve_quadratic(1, -3, 2)
-except: pass
+# Тестирование этой функции
 
-#WARNING
-try: solve_quadratic(1, 1, 10)
-except: pass
 
-#CRITICAL
-try: solve_quadratic("строка", 1, 1)
-except Exception as e: pass
+# INFO
+try:
+    solve_quadratic(1, -3, 2)
+except:
+    pass
 
-#ERROR
-try: solve_quadratic(0, 5, 1)
-except Exception as e: pass
+# WARNING
+try:
+    solve_quadratic(1, 1, 10)
+except:
+    pass
 
+# CRITICAL
+try:
+    solve_quadratic("строка", 1, 1)
+except Exception as e:
+    pass
+
+# ERROR
+try:
+    solve_quadratic(0, 5, 1)
+except Exception as e:
+    pass
--- lab7_API/lab7Test.py
+++ lab7_API/lab7Test.py
@@ -5,45 +5,51 @@ import lab7Code as main
 
 class TestGetCurrencies(unittest.TestCase):
 
-    #Тест: Одна корректная валюта
+    # Тест: Одна корректная валюта
     def test1(self):
         result = main.get_currencies(['USD'])
         self.assertIsInstance(result, dict)
         self.assertIsInstance(result['USD'], (int, float))
 
-    #Тест: Несколько валют
+    # Тест: Несколько валют
     def test2(self):
-        result = main.get_currencies(['USD','EUR'])
+        result = main.get_currencies(['USD', 'EUR'])
         self.assertEqual(len(result), 2)
 
-    #Тест: Неправильная валюта
+    # Тест: Неправильная валюта
     def test3(self):
         with self.assertRaises(KeyError):
             main.get_currencies(['абвг'])
 
-    #Тест: Неправильный адрес
+    # Тест: Неправильный адрес
     def test4(self):
         with self.assertRaises(ConnectionError):
             main.get_currencies(['USD'], url='http/////')
 
+
 class TestDecorator(unittest.TestCase):
-    #Тест: Успешная работа функции
+    # Тест: Успешная работа функции
     def test1(self):
         self.stream = io.StringIO()
+
         @main.logger(handle=self.stream)
         def test_function(x):
             return x ** 2
+
         test_function(2)
         content = self.stream.getvalue()
         self.assertIn("INFO: Запуск функции test_function с аргументами (2,) {}\n"
-                      f"INFO: Функция test_function успешно завершена. Результат: 4",content)
-    #Тест: Ошибка в работе функции
+                      f"INFO: Функция test_function успешно завершена. Результат: 4", content)
+
+    # Тест: Ошибка в работе функции
     def test2(self):
         self.stream = io.StringIO()
+
         @main.logger(handle=self.stream)
         def test_function(x):
             return x ** 2
+
         with self.assertRaises(TypeError):
             test_function('абв')
         content = self.stream.getvalue()
-        self.assertRegex(content,"ERROR")
+        self.assertRegex(content, "ERROR")
